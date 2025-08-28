4d1844b60ac587ad688fc594aea39b3e7b559b00
kangnivi yaovi sogbali <sogbalik9@gmail.com>
2025-08-27 22:00:10 -0300

    atualizacao

--- clinic_api-main/src/app.js
+++ clinic_api-main/src/app.js
@@ -1,50 +1,26 @@
-import express from "express";
-import { prismaClient } from "../prisma/prisma.js";
+import express from 'express';
+import { usuarioRouter } from './usuarios.js';
+import { exameRouter } from './exame.js';
+import { pacientesRouter } from './pacientes.js';
+import { prontuarioRouter } from './prontuario.js';
+import { consultasRouter } from './consulta.js';
 
-const app = express();
-app.use(express.json());
+export const app = express()
+app.use(express.json())
 
-app.get("/usuarios", async (request, response) => {
-  try {
-    const usuarios = await prismaClient.usuario.findMany();
-    return response.json(usuarios);
-  } catch (e) {
-    console.log(e);
-  }
-});
-app.get("/usuarios/:id", async (request, response) => {
-  try {
-    const usuario = await prismaClient.usuario.findUnique({
-      where: {
-        id: Number(request.params.id),
-      },
-    });
-    if (usuario == null) {
-      response.status(404).json("Usuario não encontrado!!");
-    }
+// rotas usuario
+app.use(usuarioRouter);
 
-    return response.json(usuario);
-  } catch (e) {
-    console.log(e);
-  }
-});
-app.post("/usuarios", async(req, res)=> {
-  try {
-    const { body } = req
-    const usuario = await prismaClient.usuario.create({
-      data: {
-        nome: body.nome,
-        cargo: body.cargo,
-        email: body.email,
-        senha: body.senha
-      },
-    })
-    return res.status(201).json(usuario)
-  } catch (error) {
-    console.error(error)
-    if(error.code === "P2002"){
-      res.status(404).send("Falha ao cadastrar usuário: Email já cadastrado!")
-    }
-  }
-})
-app.listen(3000, () => console.log("Api rodando"));
+// rotas exames
+app.use(exameRouter);
+
+// rotas pacientes
+app.use(pacientesRouter);
+
+// rotas prontuario
+app.use(prontuarioRouter);
+
+// rotas consulta
+app.use(consultasRouter);
+
+app.listen(3000, () => console.log("Api rodandos"))
